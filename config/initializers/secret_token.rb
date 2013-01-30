ff049f32b7789df6628023f934d07cc6b69eefdb
Bala Paranj <bparanj@gmail.com>
2013-01-30 00:00:28 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marsh::Application.config.secret_token = 'e9d20f6158f89a9ed2fe7dd6f6d34506bf2f141123ab08b2e289fa504131e8da6a0fcaac8574395fef4db32fde9c1460a1f64682e8cf477666e004b78ddd6b82'
